0d0a0375c3fb311d16adc1d4d01990f87073579d
Gargi Maheshwari <gargimaheshwari@users.noreply.github.com>
2020-01-08 17:42:03 +0530

    Cleaned up open-close file

--- program.py
+++ program.py
@@ -3,33 +3,31 @@ from shutil import copy
 from datetime import date
 from functions import clean_str, update_times, change_message, log
 
-f = open("list_of_files.txt", "r")
-list_files = f.readlines()
-f.close()
+with open("list_of_files.txt", "r") as f:
+    list_files = f.readlines()
 
 files = []
-for file in list_files:
-    files.append(clean_str(file))
+for f in list_files:
+    files.append(clean_str(f))
 
-f = open("last_edit_times.txt", "r")
-times = f.readlines()
-f.close()
+with open("last_edit_times.txt", "r") as f:
+    times = f.readlines()
 
 changed = False
 messages = []
 
-for i, file in enumerate(files):
-    file = clean_str(file)
-    new_time = os.path.getmtime(file)
+for i, f in enumerate(files):
+    f = clean_str(f)
+    new_time = os.path.getmtime(f)
     old_time = float(times[i])
-    name = os.path.basename(file)
+    name = os.path.basename(f)
     if new_time > old_time:
         changed = True
-        name = os.path.basename(file)
+        name = os.path.basename(f)
         fcopy = os.path.join("file_versions", name)
         d = date.fromtimestamp(new_time).strftime("%d.%m.%Y")
-        messages.append(change_message(file, fcopy, d, name))
-        copy(file, "file_versions")
+        messages.append(change_message(f, fcopy, d, name))
+        copy(f, "file_versions")
 
 if changed:
     log(messages)
